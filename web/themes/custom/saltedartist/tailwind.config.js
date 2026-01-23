16a0fa444e5a058aeae81ad48eee905ca933ac8f
ischelzel <ischelzel@augustash.com>
2026-01-23 11:34:02 -0600

    Tailwind init

---
+++ web/themes/custom/saltedartist/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} 
+ *
+*/
+module.exports = {
+  content: [
+    './templates/**/*.html.twig',
+    './js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
